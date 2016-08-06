eb37b2c56fee99f40e8652430597a6698beecdd9
Sébastien Ohleyer <sebastienohleyer@pc45.home>
2016-08-06 19:31:15 +0200

    creation de auth file

--- auth.py
+++
@@ -1,4 +0,0 @@
-string consumer_key = 'EeIZwzgWipVdJ7gkRuL6BwthN'
-string consumer_secret = 'VhPXvlazXQgKfKdsjMAXGx085TbMPaRx31FZFvG6W2o6PyyXOE'
-string access_token = '4474411877-vTCBajU7sKxX6CDISlUx7U9oeDwHr1NZ1IUKpL4'
-string acces_token_secret = 'zrULtzJ5VgR61muo6Bo7sEnGnyeRzNnisEWNHmTKR88Rj'
