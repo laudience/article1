05d4ab62cde2b8372bc677ef22f0bcefa4ffc539
Sébastien Ohleyer <sebastienohleyer@pc45.home>
2016-08-07 16:34:11 +0200

    Mentions and retweets import

--- politics.py
+++ politics.py
@@ -12,12 +12,18 @@ assemblee_nationale = api.get_user('AssembleeNat')
 for user in tweepy.Cursor(api.list_members, assemblee_nationale.screen_name, 'les-deputes').items():
     deputies_list.append(user.name)
 
-links=[]
-for deputy in deputies_list[:5]:
-    for tweet in api.user_timeline(id=deputy,count=5):
-        for mention in tweet.entities['user_mentions']:
-            links.append((deputy,mention['screen_name']))
-print(links)
+mentions_couple=[]
+retweet_couple=[]
+i=0
+for deputy in deputies_list[102:106]:
+    for tweet in tweepy.Cursor(api.user_timeline, id=deputy).items(200):
+        if 'retweeted_status' in dir(tweet):
+            retweet_couple.append((deputy, tweet.retweeted_status.user.name, tweet.created_at.date(),tweet.id))
+        else:
+            for mention in tweet.entities['user_mentions']:
+                mentions_couple.append((deputy,mention['screen_name'], tweet.created_at.date(),tweet.id))
+        i+=1
+print("\n Retweet :",retweet_couple, "\n Mentions :", mentions_couple,"\n",i)
 
 
 
