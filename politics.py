dabc4075d3090ff93f3c432001b2f3b1e0ef6d3b
Guillaume <glm.larcher@gmail.com>
2016-08-07 19:18:41 +0200

    modified .gitignore and politics

--- politics.py
+++ politics.py
@@ -1,11 +1,6 @@
-from auth.auth import consumer_key,consumer_secret, access_token, access_token_secret
-import tweepy
+from connection import *
 
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
+api = get_API()
 
 deputies_list=[]
 assemblee_nationale = api.get_user('AssembleeNat')
@@ -15,7 +10,7 @@ for user in tweepy.Cursor(api.list_members, assemblee_nationale.screen_name, 'le
 mentions_couple=[]
 retweet_couple=[]
 i=0
-for deputy in deputies_list[102:106]:
+for deputy in deputies_list[3:4]:
     for tweet in tweepy.Cursor(api.user_timeline, id=deputy).items(200):
         if 'retweeted_status' in dir(tweet):
             retweet_couple.append((deputy, tweet.retweeted_status.user.name, tweet.created_at.date(),tweet.id))
