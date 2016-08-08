400bb312941734bf6fc382008c3892875d0d1e28
Sébastien Ohleyer <sebastienohleyer@pc45.home>
2016-08-08 21:45:36 +0200

    Tentative pour recuperation de donnees sur retweet puis ecriture dans csv. Impossible à tester car code 503 sur Twitter API

--- politics.py
+++ politics.py
@@ -6,25 +6,31 @@ class PoliticsRelations:
     def __init__(self):
         self.api = get_API()
         self.deputies_dict=dict()
+        self.retweets_dict=dict()
 
     def import_data_from_csv(self):
         with open('data.csv', 'r', encoding='utf8') as csvfile:
             reader = csv.reader(csvfile, delimiter=';')
             for row in reader:
-                self.deputies_dict[row[0]]=dict()
-                self.deputies_dict[row[0]]['id_str']=row[1]
+                self.deputies_dict[row[1]]=dict() # importe les relations id_str -> screen_name pour eviter d'avoir à relire le csv à chaque fois
+                self.deputies_dict[row[1]]=row[0]
+                self.retweets_dict[row[1]]=dict()
+                for row2 in reader:
+                    self.retweets_dict[row[1]][row2[1]]=0# relations_dict[depute1][depute2]=nombre de retweet du depute2 fait par le depute1
         print("Deputies datas in dictionnary")
 
-    def mentions_and_retweets(self):
+    def mentions_and_retweets(self, writing=False):
         self.import_data_from_csv()
         mentions_couple=[]
         retweet_couple=[]
         deputy_count=0
         tweet_count=0
-        for deputy in self.deputies_dict:
-            for tweet in tweepy.Cursor(self.api.user_timeline, id=self.deputies_dict[deputy]['id_str']).items(1):
+        for deputy in self.retweets_dict:
+            for tweet in tweepy.Cursor(self.api.user_timeline, id=self.retweets_dict[deputy]).items(2):
                 if 'retweeted_status' in dir(tweet):
-                    retweet_couple.append((deputy, tweet.retweeted_status.user.name, tweet.created_at.date(),tweet.id))
+                    retweet_couple.append((deputy, tweet.retweeted_status.user.id_str, tweet.created_at.date(),tweet.id))
+                    if tweet.retweeted_status.user.id_str in self.retweets_dict:
+                        self.retweets_dict[deputy][tweet.retweeted_status.user.id_str]+=1
                 else:
                     for mention in tweet.entities['user_mentions']:
                         mentions_couple.append((deputy,mention['screen_name'], tweet.created_at.date(),tweet.id))
@@ -34,6 +40,17 @@ class PoliticsRelations:
             print(deputy_count)
 
         print("\n Retweet :",retweet_couple, "\n Mentions :", mentions_couple,"\n")
+
+        if writing is True:
+            with open('relations', 'w') as csvfile:
+                fieldnames = [id_str for id_str in self.deputies_dict]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=";")
+
+                writer.writeheader()
+
+                for deputy in self.retweets_dict:
+                    writer.writerow(self.retweets_dict[deputy])
+            print("data imported, csv writed")
 
 
 def main():
