c2e4d2718245af6a36826af02fc2b087ac812ac3
Guillaume <glm.larcher@gmail.com>
2016-09-06 21:24:21 +0200

    modfif TwitterGraph

--- TwitterGraph.py
+++ TwitterGraph.py
@@ -7,21 +7,47 @@ class TwitterGraph:
         self.nodes = {}
         self.links = {}
 
+        self.nbItems = 10
 
+    #___processTweet____________________________________________
+    
     def processTweet(tweet): #seb
 
-    def addRetweet(source, target): #gui
+    #___addRetweet______________________________________________
+        
+    def addRetweet(source, target):
+        self.links[source][target]['retweets'] += 1
 
-    def addMention(source, target): #seb
+    #___addMention______________________________________________
+    
+    def addMention(source, target): 
+        self.links[source][target]['mention'] +=1
 
-    def initNode(filename): #gui
-
+    #___initNode________________________________________________
+    
+    def initNode(filename): 
+        with open(filename, 'r') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=";")
+            for row in reader:
+                self.nodes[row['id']] = row['user_name']
+                
+    #___readJSON________________________________________________
+    
     def readJSON(filename): #seb
 
+    #___constructGraph__________________________________________
+    
     def constructGraph(): #gui
+        for node in self.nodes:
+            for tweet in tweepy.Cursor(self.api.user_timeline, id=node).item(self.nbItems):
+                processTweet()
 
+    #___saveTwitterGraph________________________________________
+                
     def saveTwitterGraph(str_name): #seb
 
+    #___plotGraph_______________________________________________
+    
     def plotGraph(): # -----
     
     
