ea084cce516c57f8c97e9381f564a14cff62e701
Guillaume <glm.larcher@gmail.com>
2016-09-06 21:05:12 +0200

    add TwitterGraph.py

---
+++ TwitterGraph.py
@@ -0,0 +1,27 @@
+from utilitaires import *
+import json
+
+class TwitterGraph:
+    def __init__(self, api):
+        self.api = api
+        self.nodes = {}
+        self.links = {}
+
+
+    def processTweet(tweet): #seb
+
+    def addRetweet(source, target): #gui
+
+    def addMention(source, target): #seb
+
+    def initNode(filename): #gui
+
+    def readJSON(filename): #seb
+
+    def constructGraph(): #gui
+
+    def saveTwitterGraph(str_name): #seb
+
+    def plotGraph(): # -----
+    
+    
